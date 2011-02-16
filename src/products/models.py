3d13ab311025fc55f0b63689990c1c7f2fcb9dff
Ümit Öztürk <1150606004@nku.edu.tr>
2011-02-16 14:00:00 +0100

    Ckeditor is added to Products App and Truncate function is appended Admin Panel

--- src/products/models.py
+++ src/products/models.py
@@ -3,27 +3,31 @@ from django.urls import reverse
 from django.db import models
 from django.utils.safestring import mark_safe
 from mptt.models import TreeForeignKey
+from django.template.defaultfilters import truncatechars
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 
 def upload_location(instance, filename):
-	upload_path = "img/product/" + str(instance) + "\_" + str(filename)
+	upload_path = "img/product/" + str(filename)
 	return upload_path
 
+
 class Product(models.Model):
-	category 		= TreeForeignKey("categories.category", on_delete=models.CASCADE, null=True,blank=True)
-	company 		= models.ForeignKey("companies.company", on_delete=models.CASCADE, related_name="company")
-	name 			= models.CharField(max_length=140)
-	title 			= models.CharField(max_length=140)
-	price 			= models.DecimalField(max_digits=6, decimal_places=2)
+	category 		= TreeForeignKey("categories.category", on_delete=models.CASCADE, null=True,blank=True, verbose_name='Kategori')
+	company 		= models.ForeignKey("companies.company", on_delete=models.CASCADE, related_name="company", verbose_name='Firma')
+	name 			= models.CharField(max_length=140, verbose_name='Ürün Adı')
+	title 			= models.CharField(max_length=140, verbose_name='Ürün Başlığı', blank=True) # Product Explanation
+	price 			= models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Fiyat')
 	exist 			= models.BooleanField(verbose_name='Stockta Var Mı?', default=True)
 	updated 		= models.DateTimeField(auto_now=True)
 	timestamp		= models.DateTimeField(auto_now_add=True)
 	image_prod		= models.ImageField(upload_to=upload_location,
 					 		null=True,
 							width_field="width_field", 
-			 				height_field="height_field")
+			 				height_field="height_field", verbose_name='Ürün Resmi')
+	content 		= RichTextField(verbose_name='Ürün Açıklaması')
 	height_field	= models.IntegerField(default=0, blank=True)
 	width_field 	= models.IntegerField(default=0, blank=True)
 
@@ -36,4 +40,4 @@ class Product(models.Model):
 			return mark_safe('<img src="%s" style="width: 100px; height:100px;" />' % self.image_prod.url)
 		else:
 			return 'No Image Found'
-	image_tag.short_description = 'Image'		
+	image_tag.short_description = 'Resim'		
--- src/projectMarket/settings/base.py
+++ src/projectMarket/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'import_export',
     'rest_framework',
+    'ckeditor',
     'mptt',
     'accounts',
     'products',
