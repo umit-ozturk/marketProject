7fbe5e53d1fe30a4117fd001bf971bf395632262
Ümit Öztürk <1150606004@nku.edu.tr>
2018-08-17 18:40:22 +0300

    Autocomplete Form is Progressed

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,14 +1,59 @@
 from django.contrib import admin
-# Register your models here.
+import json
 from django.template.defaultfilters import truncatechars
 from .models import Product
-
+from django.conf.urls import url
+try:
+    from django.urls import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
+from django.http.response import HttpResponse, HttpResponseBadRequest
 
 
 class ProductAdmin(admin.ModelAdmin):
 	list_display = ('show_name', 'show_title', 'price', 'company', 'category', 'image_tag', )
 	list_filter = ('company', 'category')
-	search_fields = ('name', 'title', 'price', )
+	search_fields = ('name', 'title', 'price', 'slug',)
+	change_list_template = 'products/change_list.html'
+	change_form_template = 'products/change_form.html'
+	search_prefix = '__icontains'	
+
+	def get_urls(self):
+		urls = super(ProductAdmin, self).get_urls()
+		api_urls = [
+			url(r'^search/(?P<search_term>\w{0,50})$', self.search_api)
+		]
+		return api_urls + urls
+
+	def search_api(self, request, search_term):
+		if not self.search_fields:
+			return HttpResponseBadRequest(reason='Aramanızla Eşleşen Ürün Yok {}'.format(self.__name__))
+		elif not search_term:
+			return HttpResponseBadRequest(reason='Bir Sorun Oluştu.')
+		else:
+			keyword = self.search_fields[3]
+			options = {
+				keyword + self.search_prefix: search_term,
+			}
+			data = []
+
+			for instance in self.model.objects.filter(**options):
+				data.append(
+					{
+						'keyword': getattr(instance, keyword),
+						'list_url': self.get_change_list_url(self.model, instance, self.model._meta.app_label),
+					}
+				)
+
+			data = json.dumps(data)
+
+			return HttpResponse(content=data, content_type='application/json')
+
+	@staticmethod
+	def get_change_list_url(model, instance, app_label):
+		return reverse(
+			"admin:%s_%s_change" % (app_label, str(model.__name__).lower()), args=(instance.id,)
+			)
 
 	def show_title(self, obj): # Product Explanation for Admin Panel
 		return truncatechars(obj.title, 35)
@@ -18,5 +63,6 @@ class ProductAdmin(admin.ModelAdmin):
 	def show_name(self, obj): # Product Name for Admin Panel
 		return truncatechars(obj.name, 20)
 	show_name.short_description = 'Ürün Adı'		
+
 
 admin.site.register(Product, ProductAdmin)
