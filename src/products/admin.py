ced658cc8f72587d9c0d60abf0f1733f46148bab
Ümit Öztürk <1150606004@nku.edu.tr>
2018-08-23 17:45:20 +0300

    Is Added Slug Field for Foreign Key

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 import json
 from django.template.defaultfilters import truncatechars
-from .models import Product
+from .models import Product, ProductInfo
 from django.conf.urls import url
 try:
     from django.urls import reverse
@@ -10,60 +10,67 @@ except ImportError:
 from django.http.response import HttpResponse, HttpResponseBadRequest
 
 
-
 class ProductAdmin(admin.ModelAdmin):
-	list_display = ('show_name', 'show_title', 'price', 'company', 'category', 'image_tag', )
-	list_filter = ('company', 'category')
-	search_fields = ('slug', 'title', 'price', 'name',)
-	search_fields = ['slug', ]
-	search_prefix = '__icontains'
-	change_list_template = 'products/change_list.html'
+    list_display = ('show_name', 'show_title', 'price', 'company', 'category', )
+    list_filter = ('company', 'category')
+    search_fields = ('title', 'price', 'name',)
+    search_prefix = '__icontains'
+    change_list_template = 'products/change_list.html'
+    raw_id_fields = ('slug',)
 
-	def get_urls(self):
-		urls = super(ProductAdmin, self).get_urls()
-		api_urls = [
-			url(r'^search/(?P<search_term>\w{0,50})$', self.search_api)
-		]
-		return api_urls + urls
+    def get_urls(self):
+        urls = super(ProductAdmin, self).get_urls()
+        api_urls = [
+            url(r'^search/(?P<search_term>\w{0,50})$', self.search_api)
+        ]
+        return api_urls + urls
 
-	def search_api(self, request, search_term):
-		if not self.search_fields:
-			return HttpResponseBadRequest(reason='Mo search_fields defined in {}'.format(self.__name__))
-		elif not search_term:
-			return HttpResponseBadRequest(reason='Mo search term provided')
-		else:
-			keyword = self.search_fields[0]
-			options = {
-				keyword + self.search_prefix: search_term,
-			}
-			data = []
+    def search_api(self, search_term):
+        if not self.search_fields:
+            return HttpResponseBadRequest(reason='Mo search_fields defined in {}'.format(self.__name__))
+        elif not search_term:
+            return HttpResponseBadRequest(reason='Mo search term provided')
+        else:
+            keyword = self.search_fields[0]
+            print(keyword)
+            options = {
+                keyword + self.search_prefix: search_term,
+            }
+            data = []
 
-			for instance in self.model.objects.filter(**options):
-				data.append(
-					{
-						'keyword': getattr(instance, keyword),
-						'url': self.get_change_form_url(self.model, instance, self.model._meta.app_label)
-					}
-				)
+            for instance in self.model.objects.filter(**options):
+                data.append(
+                    {
+                        'keyword': getattr(instance, keyword),
+                        'url': self.get_change_form_url(self.model, instance, self.model._meta.app_label)
+                    }
+                )
 
-			data = json.dumps(data)
+                data = json.dumps(data)
 
-			return HttpResponse(content=data, content_type='application/json')
+                return HttpResponse(content=data, content_type='application/json')
 
-	@staticmethod
-	def get_change_form_url(model, instance, app_label):
-		return reverse(
-			"admin:%s_%s_change" % (app_label, str(model.__name__).lower()), args=(instance.id,)
-	)
+    @staticmethod
+    def get_change_form_url(model, instance, app_label):
+        return reverse(
+            "admin:%s_%s_change" % (app_label, str(model.__name__).lower()), args=(instance.id,)
+        )
 
-	def show_title(self, obj): # Product Explanation for Admin Panel
-		return truncatechars(obj.title, 35)
-	show_title.short_description = 'Ürün Başlığı'
-	show_title.short_description = 'Ürün Açıklaması'
+    def show_title(self, obj):  # Product Explanation for Admin Panel
+        return truncatechars(obj.title, 35)
+    show_title.short_description = 'Ürün Başlığı'
+    show_title.short_description = 'Ürün Açıklaması'
 
-	def show_name(self, obj): # Product Name for Admin Panel
-		return truncatechars(obj.name, 20)
-	show_name.short_description = 'Ürün Adı'		
+    def show_name(self, obj):  # Product Name for Admin Panel
+        return truncatechars(obj.name, 20)
+    show_name.short_description = 'Ürün Adı'
+
+
+class ProductInfoAdmin(admin.ModelAdmin):
+    fields = ['slug', 'image_prod_first', 'image_prod_second', 'image_prod_third', 'image_prod_fourth', ]
+    list_display = ('slug', 'image_prod_first',)
+    search_fields = ('slug',)
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductInfo, ProductInfoAdmin)
--- src/products/models.py
+++ src/products/models.py
@@ -1,84 +1,84 @@
-from django.conf import settings
 from django.urls import reverse
 from django.db import models
 from django.utils.safestring import mark_safe
 from mptt.models import TreeForeignKey
-from django.template.defaultfilters import truncatechars
 from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 
 def upload_location(instance, filename):
-	upload_path = "img/product/" + str(filename)
-	return upload_path
-
-class ProducCompanytManager(models.Manager):
-	def get_queryset(self):
-		super().get_queryset().filter(slug__icontains=self.slug)
+    upload_path = "img/product/" + str(filename)
+    return upload_path
 
 
 class Product(models.Model):
-	category			= TreeForeignKey("categories.category", verbose_name='Kategori', on_delete=models.CASCADE, 
-										null=True,blank=True)
-	company				= models.ForeignKey("companies.company", verbose_name='Firma', on_delete=models.CASCADE, 
-											related_name="company", null=True,  blank=True)
-	brand				= models.ForeignKey("companies.brand", verbose_name='Marka', on_delete=models.CASCADE, 
-											related_name="brand", null=True,  blank=True)
-	slug				= models.CharField('Ürün Slug/Sayaç', max_length=140, null=True,  blank=True)	
-	name				= models.CharField('Ürün Adı', max_length=140, null=True,  blank=True)
-	title				= models.CharField('Ürün Başlığı', max_length=140, null=True,  blank=True)
-	price				= models.DecimalField('Ürün Fiyatı', max_digits=6, decimal_places=2, null=True,  blank=True)
-	old_price			= models.DecimalField('Eski Ürün Fiyatı', max_digits=6, decimal_places=2, null=True,  blank=True)
-	content				= RichTextField('Ürün Açıklaması', null=True, blank=True)
-	feature				= RichTextField('Ürün Özellikleri', null=True, blank=True)
-	image_prod_first	= models.ImageField('Ürün Resmi 1', upload_to=upload_location, null=True, blank=True, 
-											width_field="width_field", height_field="height_field")
-	image_prod_second	= models.ImageField('Ürün Resmi 2', upload_to=upload_location, null=True, blank=True, 
-											width_field="width_field", height_field="height_field")
-	image_prod_third	= models.ImageField('Ürün Resmi 3', upload_to=upload_location, null=True, blank=True, 
-											width_field="width_field", height_field="height_field")	
-	image_prod_fourth	= models.ImageField('Ürün Resmi 4', upload_to=upload_location, null=True, blank=True, 
-											width_field="width_field", height_field="height_field")	
-	height_field 		= models.IntegerField('Uzunluk Değeri', default=0, blank=True)
-	width_field 		= models.IntegerField('Genişlik Değeri', default=0, blank=True)
-	created_at			= models.DateTimeField('Oluşturulma Tarihi', auto_now_add=True, editable=False)
-	updated_at			= models.DateTimeField('Güncellenme Tarihi', auto_now=True, editable=False)
+    slug = models.ForeignKey("products.productinfo", verbose_name='Ürün Slug / Sayacı', on_delete=models.CASCADE,
+                             related_name="productinfo", null=True,  blank=True)
+    category = TreeForeignKey("categories.category", verbose_name='Kategori', on_delete=models.CASCADE,
+                              null=True, blank=True)
+    company = models.ForeignKey("companies.company", verbose_name='Firma', on_delete=models.CASCADE,
+                                related_name="company", null=True,  blank=True)
+    brand = models.ForeignKey("companies.brand", verbose_name='Marka', on_delete=models.CASCADE,
+                              related_name="brand", null=True,  blank=True)
+    name = models.CharField('Ürün Adı', max_length=140, null=True,  blank=True)
+    title = models.CharField('Ürün Başlığı', max_length=140, null=True,  blank=True)
+    price = models.DecimalField('Ürün Fiyatı', max_digits=6, decimal_places=2, null=True,  blank=True)
+    old_price = models.DecimalField('Eski Ürün Fiyatı', max_digits=6, decimal_places=2, null=True,  blank=True)
+    content = RichTextField('Ürün Açıklaması', null=True, blank=True)
+    feature = RichTextField('Ürün Özellikleri', null=True, blank=True)
+    created_at = models.DateTimeField('Oluşturulma Tarihi', auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField('Güncellenme Tarihi', auto_now=True, editable=False)
 
-	objects = models.Manager() 
-	objects_company = ProducCompanytManager()
+    objects = models.Manager()
 
-	class Meta:
-		verbose_name = 'Ürün'
-		verbose_name_plural = 'Ürünler'
-		ordering = ('-created_at',)	
+    class Meta:
+        verbose_name = 'Ürün'
+        verbose_name_plural = 'Ürünler'
+        ordering = ('-created_at',)
 
-	def __str__(self):
-		return str(self.name)
+    def __str__(self):
+        return str(self.name)
 
-	def get_absolute_url(self):
-		return reverse("product:detail", kwargs={'pk':self.pk})
+    def get_absolute_url(self):
+        return reverse("product:detail", kwargs={'pk': self.pk})
 
-	def get_sale_percent(self):
-		sale_percent = int((self.old_price - self.price) / self.old_price * 100)
-		return sale_percent
+    def get_sale_percent(self):
+        sale_percent = int((self.old_price - self.price) / self.old_price * 100)
+        return sale_percent
 
-	def get_filters(self):
-		filters = self.category.category_name
-		return filters
+    def get_filters(self):
+        filters = self.category.category_name
+        return filters
 
-	def get_slug_count(self):
-		slug_count = Product.objects.filter(slug__icontains=self.slug).count()
-		return slug_count
+    def get_slug_count(self):
+        slug_count = Product.objects.filter(slug__icontains=self.slug).count()
+        return slug_count
 
-	def get_slug_detail(self):
-		slug_company_detail = Product.objects.filter(slug__icontains=self.slug)
-		print(slug_company_detail)
-		return slug_company_detail
+    def get_slug_detail(self):
+        slug_company_detail = Product.objects.filter(slug__icontains=self.slug)
+        return slug_company_detail
 
-	def image_tag(self):
-		if self.image_prod_first:
-			return mark_safe('<img src="%s" style="width: 100px; height:100px;" />' % self.image_prod_first.url)
-		else:
-			return 'No Image Found'
-	image_tag.short_description = 'Resim'
+
+class ProductInfo(models.Model):
+    slug = models.CharField('Ürün Slug / Sayacı', max_length=140, null=True, blank=True)
+    image_prod_first = models.ImageField('Ürün Resmi 1', upload_to=upload_location, null=True, blank=True,
+                                         width_field="width_field", height_field="height_field")
+    image_prod_second = models.ImageField('Ürün Resmi 2', upload_to=upload_location, null=True, blank=True,
+                                          width_field="width_field", height_field="height_field")
+    image_prod_third = models.ImageField('Ürün Resmi 3', upload_to=upload_location, null=True, blank=True,
+                                         width_field="width_field", height_field="height_field")
+    image_prod_fourth = models.ImageField('Ürün Resmi 4', upload_to=upload_location, null=True, blank=True,
+                                          width_field="width_field", height_field="height_field")
+    height_field = models.IntegerField('Uzunluk Değeri', default=0, blank=True)
+    width_field = models.IntegerField('Genişlik Değeri', default=0, blank=True)
+
+    def __str__(self):
+        return str(self.slug)
+
+    def image_tag(self):
+        if self.image_prod_first:
+            return mark_safe('<img src="%s" style="width: 100px; height:100px;" />' % self.image_prod_first.url)
+        else:
+            return 'No Image Found'
+    image_tag.short_description = 'Resim'
