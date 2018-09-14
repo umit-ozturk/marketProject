170bb8f8c6dbb483b74c9c8260f9f5e3425c8c02
Ümit Öztürk <1150606004@nku.edu.tr>
2018-09-14 19:53:17 +0300

    Search View is Done

--- products/views.py
+++ products/views.py
@@ -35,6 +35,7 @@ class ProductListView(ListView):
 class SearchView(TemplateView):
     template_name = 'products/search_product_list.html'
     tab_class = 'search'
+    model = Product
 
     partial_templates = {
         'querys': 'products/search/query.html',
@@ -53,12 +54,7 @@ class SearchView(TemplateView):
         return super(SearchView, self).dispatch(request, *args, **kwargs)
 
     def get_keywords(self):
-        print("get_keywords")
         return self.request.GET.get('keywords') or ''
-
-    def is_json(self):
-        return (self.request.is_ajax() or
-                self.request.GET.get('json'))
 
     def get_search_bundle(self):
         method = getattr(self, self.method_mapping[self.type])
@@ -66,65 +62,48 @@ class SearchView(TemplateView):
                  'object': item} for item in method()]
 
     def get_context_data(self, **kwargs):
-        return super(SearchView, self).get_context_data(results=self.get_search_bundle(), **kwargs)
+        return super(SearchView, self).get_context_data(min_price=self.get_min_price(), max_price=self.get_max_price(),
+                                                        results=self.get_search_bundle(), **kwargs)
 
     def get_next_page_url(self):
-        return '?query=%(query)s&price=%(price)s&pagination=%(pagination)s' % {
-            "query": self.get_queries(),
+        return '?keywords=%(keywords)s&price=%(price)s&pagination=%(pagination)s' % {
+            "keywords": self.get_queries(),
             "price": self.get_values(),
             "pagination": self.get_pagination()
         }
 
     def get_queries(self):
         keywords = self.request.GET.get('keywords')
-        if not keywords or len(keywords) < 2:
+        min_value, max_value = self.get_values()
+        if not keywords or len(keywords) < 1:
             result = Product.objects.none()
         else:
-            result = (Product.objects.filter(name__icontains=keywords))
+            result = (Product.objects.filter(name__icontains=keywords, price__range=(min_value, max_value)))
         return result
 
     def get_values(self):
-        print("get_values")
-        price = self.request.GET.get('keywords')
+        price = self.request.GET.get('price')
         if not price or len(price) < 1:
-            result = Product.objects.none()
-        else:
-            result = (Product.objects.filter(title__icontains=price,))
+            return self.get_min_price(), self.get_max_price()
+        query_arg = price.split("TL-")
+        min_value = price.split("TL-")[0]
+        max_value = query_arg[1].split("TL")[0]
+        return min_value, max_value
 
     def get_pagination(self):
-        print("get_pagination")
         pagination = self.request.GET.get('keywords')
-        if not pagination or len(pagination) < 2:
+        if not pagination or len(pagination) < 1:
             result = Product.objects.none()
         else:
             result = (Product.objects.filter(title__icontains=pagination,))
-
-    def render_to_response(self, context, **response_kwargs):
-        if not self.is_json():
-            return super(SearchView, self).render_to_response(
-                context, **response_kwargs)
-
-        results = [{
-                       "id": result['object'].id,
-                       "label": result['object']
-                   } for result in context['results']]
-
-        return HttpResponse(
-            json.dumps(results),
-            dict(content_type='application/json', **response_kwargs)
-        )
+        return result
 
     @staticmethod
-    def price_list_min_max():
-        price__min = round(Product.objects.aggregate(Min('price'))['price__min'])
+    def get_min_price():
+        price__min = round(Product.objects.aggregate(Max('price'))['price__max'])
+        return price__min
+
+    @staticmethod
+    def get_max_price():
         price__max = round(Product.objects.aggregate(Max('price'))['price__max'])
-        price_list = {'price__min': price__min, 'price__max': price__max}
-        return price_list
-
-
-#            query_arg = query_price.split("TL-")
-#            min_value = query_price.split("TL-")[0]
-#            max_value = query_arg[1].split("TL")[0]
-#            qs = Product.objects.filter(
-#                Q(price__range=(min_value, max_value))
-#                ).distinct()
+        return price__max
