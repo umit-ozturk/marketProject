b92388e5297c9a0172882393b88da1c11c4f3fc2
Ümit Öztürk <1150606004@nku.edu.tr>
2018-10-14 20:00:01 +0300

    Remove ignored files

--- companies/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CompaniesConfig(AppConfig):
-    name = 'companies'
-    verbose_name = 'Firmalar / Markalar'
--- tickets/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Ticket, TicketCompany
-
-# Register your models here.
-
-admin.site.register(Ticket)
-admin.site.register(TicketCompany)
