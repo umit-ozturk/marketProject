9557663b17346a596c6df21aac20847c46928b78
Ümit Öztürk <1150606004@nku.edu.tr>
2018-10-30 15:40:51 +0300

    Search

--- products/urls.py
+++ products/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 
 from .views import(
     ProductDetailView,
-    SearchView
+    SearchProductListView
     )
 
 
 app_name = 'products'
 
 urlpatterns = [
-    path('search', SearchView.as_view(), name="search-list"),  # /product/
+    path('search', SearchProductListView.as_view(), name="search-list"),  # /product/
     path('<int:pk>/', ProductDetailView.as_view(), name="detail"),  # /product/1/
 ]
--- products/views.py
+++ products/views.py
@@ -13,6 +13,7 @@ from django.db.models import Min, Max
 from django.http import HttpResponse, Http404
 import json
 
+
 class ProductDetailView(DetailView):
     queryset = Product.objects.all()
 
@@ -44,3 +45,29 @@ class ProductListView(ListView):
         return price__max
 
 
+class SearchProductListView(ListView):
+    template_name = 'products/search_product_list.html'
+    queryset = Product.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Product.objects.all()
+        query = self.request.GET.get("q", None)
+        query_price = self.request.GET.get("price", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(name__icontains=query) |
+                Q(title__icontains=query)
+            )
+        if query_price is not None:
+            query_arg = query_price.split("TL-")
+            min_value = query_price.split("TL-")[0]
+            max_value = query_arg[1].split("TL")[0]
+            qs = qs.filter(
+                Q(price__range=(min_value, max_value))
+            ).distinct()
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductListView, self).get_context_data(*args, **kwargs)
+        context['carts'] = global_cart_detail(self.request)
+        return context
