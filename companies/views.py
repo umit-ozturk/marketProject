2bdda2b54a0b66387c79dea2aac5036cf251ade7
Ümit Öztürk <1150606004@nku.edu.tr>
2019-06-04 16:07:31 +0300

    Products By Company Name

--- companies/views.py
+++ companies/views.py
@@ -3,9 +3,10 @@ from django.views.generic import (
     DetailView,
     ListView
     )
-from .models import Company
+from companies.models import Company
 from cart.views import global_cart_detail
 from products.utils import get_category_name
+from products.models import Product
 
 
 def global_companies_detail():
@@ -14,16 +15,16 @@ def global_companies_detail():
 
 
 class CompanyDetailView(DetailView):
-    queryset = Company.objects.all()
-
     def get_queryset(self, *args, **kwargs):
         qs = Company.objects.all()
         return qs
 
     def get_context_data(self, *args, **kwargs):
         context = super(CompanyDetailView, self).get_context_data(*args, **kwargs)
-        context['carts'] = global_cart_detail(self.request)
-        context['category_names'] = get_category_name()
+        context['products'] = Product.objects.filter(company__id=self.kwargs['pk'])
+        # context['carts'] = global_cart_detail(self.request)
+        # context['category_names'] = get_category_name()
+        print(context)
         return context
 
 
@@ -42,7 +43,6 @@ class CompanyListView(ListView):
                 Q(price__icontains=query) |
                 Q(title__icontains=query)
                 )
-        print(qs.count())
         return qs
 
     def get_context_data(self, *args, **kwargs):
